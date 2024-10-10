d4c54e685b226d9ae28d7273e38977986d2653bc
Clairetarus <clairejepngetich@gmail.com>
2024-10-10 23:59:45 +0300

    Add files via upload

---
+++ p.dowhile[1].c
@@ -0,0 +1,23 @@
+// do while loop for numbers
+#include <stdio.h>
+int main(){
+int start;
+int stop;
+int sum;
+
+printf("enter start");
+scanf("%d",&start);
+
+printf("enter stop");
+scanf("%d,&to stop");
+
+do{
+printf("%d/n",start);
+
+start++;
+sum+=start;
+}while (start<=stop);
+printf("the sum is %d",sum);
+
+	return 0;
+}
